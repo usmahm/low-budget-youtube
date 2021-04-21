ca981056a23186b0cb0cae7d9f567daae2d1be4a
Ahmad Usman <ahmadujuwon@gmail.com>
2021-04-21 18:00:15 +0100

    update keys

--- src/shared/APIKeys.js
+++ src/shared/APIKeys.js
@@ -2,28 +2,28 @@
 
 // Using different keys in different components to prevent reaching API call Limit early
 const APIKeys = {
-    key1: "AIzaSyC0-Cu83uFnN2GDL04ISyf8NO674ElR2P8", // Home Page
-    key2: "AIzaSyBQYPwOPrbiFmiafbPOKlxQsieNuMV31yI", // Home Page
-    key3: "AIzaSyAqeL-FB6D4o0Yd7MDEbrMSHxQR3aA5ZsA", // Trending Page
-    key4: "AIzaSyB8jSUYTpGBRH3bwbYwp844FLyr8AE9ZI4", // Trending Page
-    key5: "AIzaSyADE6JG6s_HpU-AMFTNcO_PvmE_yt4WgLc", // Search results
-    key6: "AIzaSyDVJP-R4t3T4Qj2FLMsgaAB39B4JlWvahQ", // Search results
-    key7: "AIzaSyAI3frSXVHODo5CIamVABLPp4hZM0elCkw", // WatchVid 
-    key8: "AIzaSyD03FvXujunXtD23SmDjTv-JZCbu4CceqE", // WatchVid Re-add
-    key9: " AIzaSyD0K4wMDdI_brcTYckvcfSyzeP53AE0jNk", // WatchVid /APIkey 20
-    key10: "AIzaSyD0ibybBSF9VDxGj80unEVaezAPV9DFHcU", // WatchVid 
-    key11: "AIzaSyDyGYfM6_JCa92KUOPgQYuHR5j6keRiU7Y", // useSearchHttp
-    key12: "AIzaSyDe-yDYwDUvXC2qWZ01syvRajCuFy-ss2k", // useSearchHttp /key 12
-    key13: "AIzaSyDMq3VS0aWYcO2EZV9lQ-2QqSUrDpbma0Q", // useSearchHttp
-    key14: "AIzaSyD_E-HIVl_UNNE5GF4DgnBhS4NaySYlfEs", // useSearchHttp /key 7
-    key15: "AIzaSyCwUfYcjRojmjq_kirm6ZNNkLkvQYQ6vCA", // Channel Videos
-    key16: "AIzaSyBsBc1ONPmVrfnlakYpZC55fjHTBOptebQ", // Channel Videos
-    key17: "AIzaSyBfF8wZ4VuydCPr-zWP7cCD4lEiX9BafbY", // Channel /key 8
-    key18: "AIzaSyBal0CGG54jiOoURCXTSSwsO2r0ALyYrZ0", // useVideoHttp /key 11
-    key19: "AIzaSyASz3eN8oXJ3BYlD62LQCJ8ClKkFpxVbns", // useVideoHttp /key 10
+    key1: "AIzaSyAtKTmZIeGyrQghjivro-zE0uw91ZbhMtc", // Home Page
+    key2: "AIzaSyBi0YP89fJCBXff2f3yJkSP2pWLb3AY_fM", // Home Page
+    key3: "AIzaSyBz5mvOQMOHu37e5v8P-S0FJd9PBcQLN_M", // Trending Page
+    key4: "AIzaSyAfngwd9QrlLFJewVx44hVDp3Seof8bBk4", // Trending Page
+    key5: "AAIzaSyAfngwd9QrlLFJewVx44hVDp3Seof8bBk4", // Search results
+    key6: "AIzaSyDVhWlSxO3aXw3tqevA6VJdS7qdfQuPpFA", // Search results
+    key7: "AIzaSyBWL_VsGMC_ZrlsI2yEAh2xzpYn5VZpgCA", // WatchVid 
+    key8: "AIzaSyBFv4mugqCdLh5NlZiSHtSP2UaNRN-vFuQ", // WatchVid Re-add
+    key9: "AIzaSyCbvY0-VyJ3N9ANF63D1B-XjKNE7knjKvI", // WatchVid /APIkey 20
+    key10: "AIzaSyDgVIyaAGXZK_YPEc79SCcL816uCglv9Hs", // WatchVid 
+    key11: "AIzaSyDjNFJMRmKAm94AFcp2_6JyKlMb8rkrXSw", // useSearchHttp
+    key12: "AIzaSyCVYdQSo0PCQwDxPRNWWc6P6TJNk5XY9fk", // useSearchHttp /key 12
+    key13: "AIzaSyBb1gshgwbzjRVc3ob2R08hYaQQC_tyDfc", // useSearchHttp
+    key14: "AIzaSyDfnWE9Ei4hw98SYzSw0cywqUS_iqf8mSA", // useSearchHttp /key 7
+    key15: "AIzaSyDwhvbQVJ1lL2NgM8yFQAVrR3y8_VTb4uw", // Channel Videos
+    key16: "AIzaSyCQaOwdlrca0zhM-P9m_KyEk8n8K8s6LkY", // Channel Videos
+    key17: "AIzaSyCrYwxuLA7BLBm5XIZ9fcgg99GgvCVtrFg", // Channel /key 8
+    key18: "AIzaSyBKlSeQWHk-mOSNqb1v0FUxpTC2GUG8SXw", // useVideoHttp /key 11
+    key19: "AIzaSyBCn1guyKf7_0A8I0VL16RsbhJjDs-QQPA", // useVideoHttp /key 10
 
-    key20: "AIzaSyD0K4wMDdI_brcTYckvcfSyzeP53AE0jNk", //
-    exhaust: "AIzaSyD-o-aKL9q8Zh25uYSRZAj-KQQu8UVHFY4",
+    key20: "AIzaSyCP3i-DKfFELARAPvnU80_C6GfxRhgKFIA", //
+    exhaust: "AIzaSyDh-5xHi4jFKOQnsTHKOFVCtUor0XXJgYM",
 }
 
 export default APIKeys;
